99666c0d3de08c897b9bd6e559f65a5227bf60d8
mythosil <mythosil@yahoo.co.jp>
2013-09-27 00:22:50 +0900

    add provider github search

---
+++ provider.js
@@ -0,0 +1,12 @@
+/* firefox only */
+
+function addProvider(xmlname) {
+  var url = document.URL;
+  var absPath = url.substring(0, url.lastIndexOf("/")) + "/provider/" + xmlname;
+  window.external.AddSearchProvider(absPath);
+}
+
+function addGitHubSearch() {
+  addProvider("github.xml");
+}
+
